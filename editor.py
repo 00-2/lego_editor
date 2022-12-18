6e3550cd38f83b001e7dcc69273af162bc62ba66
00-2 <9x02@mail.ru>
2022-12-19 02:46:31 +0300

    add support of light sources rotation

--- editor.py
+++ editor.py
@@ -1,4 +1,7 @@
+import numpy
 from OpenGL.GL import *
+from numpy import cos, sin
+
 from shader import Shader
 import os
 import numpy as np
@@ -20,9 +23,14 @@ class Editor:
         self.camera_location = glGetUniformLocation(self.program.program, "camera")
         self.part_color_location = glGetUniformLocation(self.program.program, "partColor")
         self.light_position_location = glGetUniformLocation(self.program.program, "lightPosition")
+        self.light1_position_location = glGetUniformLocation(self.program.program, "light1Position")
+        self.light_status = glGetUniformLocation(self.program.program, "lightStatus")
+        self.light1_status = glGetUniformLocation(self.program.program, "light1Status")
         self.dim_location = glGetUniformLocation(self.program.program, "dim")
         self.camera = camera
-
+        self.rotate_lights = 0
+        self.rotate_lights_angle = 0
+        self.rotate_lights_radius = 5
         self.active_part = Part()
         self.active_type = 0
    
@@ -35,7 +43,7 @@ class Editor:
             self.parts = editor_save["parts"]
             self.parts = [[Part.from_json(part) for part in parts] for parts in self.parts]
             self.part_history = editor_save["part_history"]
-        
+
         self.colors = np.array([[222, 0, 13],
                        [0, 87, 168],
                        [242, 205, 55],
@@ -43,7 +51,10 @@ class Editor:
                        [255, 255, 255],
                        [5, 19, 29]], dtype="float32") / 256
 
-        self.light_pos = np.array([0, 10, 0], dtype="float32")
+        # self.light_pos = np.array([0, 10, 0], dtype="float32")
+        ((l1x, l1y, l1z), (l2x, l2y, l2z)) = ((10, 0, 10), (-10, 0, -10))
+        self.light_positions = np.array([[l1x, l1y, l1z], [l2x,l2y,l2z]], dtype="float32")
+        self.light_statuses = [1.0, 1.0]
         self.light_dim = 0.98
 
         self.cube_vao = glGenVertexArrays(1)
@@ -78,21 +89,33 @@ class Editor:
             json.dump({"parts": [[part.to_dict() for part in parts] for parts in self.parts], "part_history": self.part_history}, file)
     def render(self, window_size):
         glUseProgram(self.program.program)
-        
+        if self.rotate_lights:
+            print('render', self.rotate_lights_angle)
+            ((l1x, l1y, l1z), (l2x, l2y, l2z)) = (
+                (
+                    self.rotate_lights_radius * cos(self.rotate_lights_angle)+self.rotate_lights_radius*sin(self.rotate_lights_angle),
+                    0,
+                    -self.rotate_lights_radius*sin(self.rotate_lights_angle)+self.rotate_lights_radius * cos(self.rotate_lights_angle)
+                ),
+                (
+                    - (self.rotate_lights_radius * cos(self.rotate_lights_angle) + self.rotate_lights_radius * sin(self.rotate_lights_angle)),
+                    0,
+                    - (-self.rotate_lights_radius * sin(self.rotate_lights_angle) + self.rotate_lights_radius * cos(self.rotate_lights_angle))
+                )
+            )
+            self.rotate_lights_angle+=numpy.pi/1800
+            self.light_positions = np.array([[l1x, l1y, l1z], [l2x, l2y, l2z]], dtype="float32")
         glUniformMatrix4fv(self.camera_location, 1, GL_FALSE, self.camera.get_matrix(window_size[0] / window_size[1]))
-        glUniform3fv(self.light_position_location, 1, self.light_pos)
+        glUniform3fv(self.light_position_location, 1, self.light_positions[0])
+        glUniform3fv(self.light1_position_location, 1, self.light_positions[1])
         glUniform1f(self.dim_location, self.light_dim)
+        glUniform1f(self.light_status, self.light_statuses[0])
+        glUniform1f(self.light1_status, self.light_statuses[1])
 
         glBindVertexArray(self.cube_vao)
         glUniform3f(self.part_color_location, 100, 100, 100)
-        glUniformMatrix4fv(self.model_location, 1, GL_FALSE, np.array([
-                [1, 0, 0, -self.light_pos[0]],
-                [0, 1, 0, -self.light_pos[1]],
-                [0, 0, 1, -self.light_pos[2]],
-                [0, 0, 0, 1]
-            ], dtype="float32").T)
+
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 14)
-        
         for part_type, parts in enumerate(self.parts):
             Part.render_many(part_type, parts, self)
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
@@ -108,14 +131,14 @@ class Editor:
         for part_type, parts in enumerate(self.parts):
             for part in parts:
                 if intersect(active_rect, part.rect(part_type)):
-                    return
+                    print("not intersect")
+                    return 0
         
         self.parts[self.active_type].append(self.active_part)
         self.part_history.append(self.active_type)
         self.active_part = Part(self.active_part.pos.copy())
-
         self.save()
-        
+        return 1
     def undo(self):
         if len(self.part_history) == 0:
             return
@@ -184,6 +207,49 @@ class Camera:
         self.forward = np.array([x, y, z], dtype="float32")
         self.forward /= np.linalg.norm(self.forward)
 
+
+
+def centroid(vertexes):
+    _x_list = [vertex[0] for vertex in vertexes]
+    _y_list = [vertex[1] for vertex in vertexes]
+    _z_list = [vertex[2] for vertex in vertexes]
+    _len = len(vertexes)
+    _x = sum(_x_list) / _len
+    _y = sum(_y_list) / _len
+    _z = sum(_z_list) / _len
+    return(_x, _y, _z)
+
+
+def in_shadow(part, part_types, light_position, parts):
+    sizes = part.sizes[part_types]
+    rect_sample = [
+        [0, 0, 0],
+        [0, -sizes[1], 0],
+        [0, -sizes[1], sizes[2]],
+        [0, 0, sizes[2]],
+        [-sizes[0], 0, 0],
+        [-sizes[0], -sizes[1], 0],
+        [-sizes[0], -sizes[1], sizes[2]],
+        [-sizes[0], 0, sizes[2]]
+    ]
+    rect = [[_[0] + part.pos[0], _[1] + part.pos[1], _[2] + part.pos[2]] for _ in rect_sample]
+    _surface_points = [(pow(point[0] - light_position[0], 2) + pow(point[1] - light_position[1], 2) + pow(
+        point[2] - light_position[2], 2), i) for i, point in enumerate(rect)]
+    _surface_points.sort()
+    surface = [rect[_point[1]] for _point in _surface_points[:3]]
+    center_point = centroid(surface)  # координаты нормали к поверхности
+    parametric_equation = center_point - light_position[0]  # i,j,k equation
+    for _ in parts:
+        for _part in _:
+            if part.pos == _part.pos:
+                break
+            rect = [[_[0] + _part.pos[0], _[1] + _part.pos[1], _[2] + _part.pos[2]] for _ in rect_sample]
+            for surface in rect:
+                if abs(-(parametric_equation[0]*surface[0] + parametric_equation[1]+surface[1])/parametric_equation[2]-surface[2]) < 0.01 or \
+                    abs(-(parametric_equation[0] * surface[0] + parametric_equation[2] + surface[2]) /parametric_equation[1] - surface[1]) < 0.01 or \
+                    abs(-(parametric_equation[1] * surface[1] + parametric_equation[2] + surface[2]) / parametric_equation[0] - surface[0]) < 0.01:
+                    return True
+    return False
 class Part:
     initialized = False
     part_types = 0
@@ -191,6 +257,7 @@ class Part:
     buffers = []
     elements_counts = []
     sizes = []
+    faces = []
     def static_init():
         if Part.initialized:
             return
@@ -204,23 +271,33 @@ class Part:
         Part.buffers = glGenBuffers(alloc)
 
         Part.elements_counts = [0 for i in range(alloc)]
+        Part.faces = [0 for i in range(alloc)]
         Part.sizes = [0 for i in range(alloc)]
 
         for part_type, part in enumerate(parts):
             mesh = Mesh(os.path.join(os.path.dirname(__file__), "parts", part["file"]), Part.vertex_arrays[part_type], Part.buffers[part_type], part["size"], part["pin"])
             Part.sizes[part_type] = part["size"]
             Part.elements_counts[part_type] = mesh.count
+            Part.faces[part_type] = mesh.result
 
     def render_many(part_type, parts, editor):
         glBindVertexArray(Part.vertex_arrays[part_type])
-        for part in parts:
-            model = part.model_matrix()
-            glUniformMatrix4fv(editor.model_location, 1, GL_FALSE, model)
-            glUniformMatrix3fv(editor.model_normal_location, 1, GL_FALSE, np.linalg.inv(model[:-1,:-1]).transpose())
+        for light_position in editor.light_positions:
 
-            glUniform3fv(editor.part_color_location, 1, editor.colors[part.color % len(editor.colors)])
-            
-            glDrawArrays(GL_TRIANGLES, 0, Part.elements_counts[part_type])
+            for part in parts:
+                # algorithm. If faces vector intersect more than one part, any other part not highlighted
+                model = part.model_matrix()
+                glUniformMatrix4fv(editor.model_location, 1, GL_FALSE, model)
+                # if not in_shadow(part, part_type, light_position,editor.parts):
+                #     #set light_position to 0
+                #     glUniform1f(editor.light_status, 0.1)
+                # else:
+                #     #set dim to 0.2
+                #     glUniform1f(editor.light_status, 1)
+                glUniformMatrix3fv(editor.model_normal_location, 1, GL_FALSE, np.linalg.inv(model[:-1,:-1]).transpose())
+                glUniform3fv(editor.part_color_location, 1, editor.colors[part.color % len(editor.colors)])
+
+                glDrawArrays(GL_TRIANGLES, 0, Part.elements_counts[part_type])
 
     def __init__(self, pos=[0, 0, 0]):
         self.pos = pos
@@ -252,4 +329,4 @@ class Part:
         x2 = self.pos[0] + size[0]
         y2 = self.pos[1] + size[1]
         z2 = self.pos[2] + size[2]
-        return (min(self.pos[0], x2), max(self.pos[0], x2), self.pos[1], y2, min(self.pos[2], z2), max(self.pos[2], z2))
+        return (min(self.pos[0], x2), max(self.pos[0], x2), min(self.pos[1], y2), max(self.pos[1], y2), min(self.pos[2], z2), max(self.pos[2], z2))
